6c479933b794adb58d756b0177fc00f6bf728cd6
Daniel <daniel.voronezhskiy@gmail.com>
2020-06-17 04:07:50 +0300

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,43 +1,61 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded'
-import { useSpring, animated } from 'react-spring'
+import { useTransition, useSpring, animated } from 'react-spring'
 import '../layoutStyles/Navbar.scss'
 
 function Navbar(props) {
    
-    const [burgerState, toggleState] = useState(false)
+    const [burgerState, setBurgerState] = useState(false)
 
-    // animation settings
-    props = useSpring({
-        opacity: burgerState ? 1: 0
+
+    // animation for burger icon rotation
+    const { degree } = useSpring({
+        from: { degree: 0 },
+        degree: burgerState ? 360 : 0
+    })
+
+    // animation settings for 
+    // burger insides appearance
+    const transitionForOpacity = useTransition(burgerState, null, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 }
     })
     
 
     return(
         <div className="navbar-container" >
             <h1 id="site-name" >The Name</h1>
-            <MenuRoundedIcon
-                id="burger-icon"
-                style={{ 
-                    zIndex: 2,
-                    margin: "1.5rem 1.25rem 0 0",
-                    width: "2rem",
-                    height: "2rem"
+            <animated.div 
+                id="burger-container"
+                style={{
+                    transform: degree.interpolate(degree => rotate(${degree}deg)),
+                    opacity: 1
                 }}
-                onClick={() => toggleState(!burgerState)}
-            />
-            {burgerState
-            ? <animated.div 
-                id="link-container"
-                style={props}
             >
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/faq">F.A.Q.</Link>
-             </animated.div>
-            : null
-            }
+                <MenuRoundedIcon
+                    id="burger-icon"
+                    style={{ 
+                        zIndex: 2,
+                        margin: "1.5rem 1.25rem 0 0",
+                        width: "2rem",
+                        height: "2rem"
+                    }}
+                    onClick={() => setBurgerState(!burgerState)}
+                />
+            </animated.div>
+            {transitionForOpacity.map(({item, key, props}) => (
+                item && 
+                    <animated.div 
+                        id="link-container"
+                        style={props}
+                    >
+                        <Link to="/about">About</Link>
+                        <Link to="/contact">Contact</Link>
+                        <Link to="/faq">F.A.Q.</Link>
+                    </animated.div>
+            ))}
             
         </div>
     )
